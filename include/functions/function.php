311c75a29387263c790a7bc4604a2b0ba9d7eac5
Dalsukh Parmar <dalsukhp15.iipl@gmail.com>
2017-02-18 00:11:16 +0530

    mail by gmail

--- include/functions/function.php
+++ include/functions/function.php
@@ -647,11 +647,39 @@ function sendMail($from="parmar.dalsukh@gmail.com",$to="",$subject="")
 	$mail = new PHPMailer;
 	//Tell PHPMailer to use SMTP
 	//$mail->isSendmail();
+	//Tell PHPMailer to use SMTP
 	$mail->isSMTP();
-	$mail->Host = 'mx1.hostinger.nl';  // Specify main and backup SMTP servers
-	$mail->SMTPAuth = false;                              
 
+	//Enable SMTP debugging
+	// 0 = off (for production use)
+	// 1 = client messages
+	// 2 = client and server messages
+	$mail->SMTPDebug = 2;
+
+	//Ask for HTML-friendly debug output
+	$mail->Debugoutput = 'html';
+
+	//Set the hostname of the mail server
+	$mail->Host = 'smtp.gmail.com';
+	// use
+	// $mail->Host = gethostbyname('smtp.gmail.com');
+	// if your network does not support SMTP over IPv6
+
+	//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
 	$mail->Port = 587;
+
+	//Set the encryption system to use - ssl (deprecated) or tls
+	$mail->SMTPSecure = 'tls';
+
+	//Whether to use SMTP authentication
+	$mail->SMTPAuth = true;
+
+	//Username to use for SMTP authentication - use full email address for gmail
+	$mail->Username = "parmar.dalsukh@gmail.com";
+
+	//Password to use for SMTP authentication
+	$mail->Password = "P20@DilkumaR";
+
 		
 	//Set who the message is to be sent from
 	$mail->setFrom($from, 'Dalsukh Parmar');
